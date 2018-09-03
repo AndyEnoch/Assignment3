e6ffd88a09c413457a4bbf36a4ce4a6d8038a0ce
Fhama-A <40128406+Fhama-A@users.noreply.github.com>
2018-09-03 10:07:43 +0000

    Add files via upload

---
+++ A3_10675104.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+    cout<<"Enter marks here: "<<endl;
+    cin>> marks;
+    
+    if(marks>=80 && marks<=100)
+        cout<<"\nGRADE A ";
+        
+    if(marks>=75 && marks<=79)
+        cout<<"\nGrade B+ ";
+    
+    if(marks>=70 && marks<=74)
+        cout<<"\nGrade B ";
+        
+    if(marks>=65 && marks<=69)
+        cout<<"\nGrade C+ ";
+        
+    if(marks>=60 && marks<=64)
+        cout<<"\nGrade C ";
+     
+    if(marks>=55 && marks<=59)
+        cout<<"\nGrade D+ ";
+       
+    if(marks>=50 && marks<=54)
+        cout<<"\nGRade D+ ";
+        
+    if(marks>=45 && marks<=49)
+        cout<<"\nGrade E ";
+        
+    if(marks>=0 && marks<=44)
+        cout<<"Grade F ";
+    
+    
+        
+   
+ 
+ 
+   
+    
+    
+    
+    
+    
+	cout<<""<< endl;
+    
+    
+    
+    
+	return 0;
+}
